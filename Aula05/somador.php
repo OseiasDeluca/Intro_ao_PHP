b6086767bb43ad17028f81cea2515cdf13a23b80
OseiasDeluca <89862166+OseiasDeluca@users.noreply.github.com>
2021-12-15 00:08:31 -0300

    Adicionando mais operações

--- Aula05/somador.php
+++ Aula05/somador.php
@@ -15,6 +15,10 @@
         $n2 = 2; // Variavél do segundo valor
         $s = $n1 + $n2; // // Variavél responsaável p/ receber o valor da primeira variável e da segunda
         echo "A soma entre $n1 e $n2 é = $s"; // Mostra no navegador
+        echo "<br/>A subtracao vale ". ($n1-$n2);
+        echo "<br/>A multiplicacao vale ". ($n1*$n2);
+        echo "<br/>A divisao vale ". ($n1/$n2);
+        echo "<br/>A smodulo vale ". ($n1%$n2);
     ?> 
 </div>
 </body>
